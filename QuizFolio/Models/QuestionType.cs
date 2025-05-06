daffafe155c85e6cb115d9ff73e7f4c24349ea20
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-05-06 20:13:08 +0600

    add multiline text in template

--- QuizFolio/Models/QuestionType.cs
+++ QuizFolio/Models/QuestionType.cs
@@ -1,8 +1,14 @@
-﻿namespace QuizFolio.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizFolio.Models
 {
     public enum QuestionType
     {
+        [Display(Name = "Single Line Text")]
         Text,
+        [Display(Name = "Multi Line Text")]
+        TextArea,
         Dropdown,
         Radio,
         Checkbox,
